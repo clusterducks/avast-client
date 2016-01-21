1a0225b8931658438bed3fdbe0297ced55490893
bfowle <brettfowle@gmail.com>
2016-01-21 14:53:59 -0500

    begin ui refresh

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'avast-sidebar',
+  template: require('./sidebar.component.html'),
+  styles: [
+    require('./sidebar.component.css')
+  ]
+})
+
+export class SidebarComponent {
+}
