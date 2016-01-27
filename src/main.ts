ea1f8ff394469e21effc0e1851c5db66b15b5051
bfowle <brettfowle@gmail.com>
2016-01-27 15:06:34 -0500

    html5 pushstate disable

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {provide} from 'angular2/core';
 import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {createStore, combineReducers, bindActionCreators, applyMiddleware} from 'redux';
 const thunkMiddleware = require('redux-thunk');
@@ -49,6 +49,7 @@ document.addEventListener('DOMContentLoaded', function main() {
     ...(process.env.ENV === 'production' ? [] : ELEMENT_PROBE_PROVIDERS),
     ...HTTP_PROVIDERS,
     ...ROUTER_PROVIDERS,
+    provide(LocationStrategy, {useClass: HashLocationStrategy}),
     provide(AppStore, {useValue: appStore}),
     NodesService,
     ConsulActions,
